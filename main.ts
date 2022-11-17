0e652c33f89ea4ceada76f33a2a232b28669e063
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-11-16 23:13:10 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,14 @@
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(input.magneticForce(Dimension.Strength))
 })
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "Door Open") {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 5000, 5000, 255, 255, 5000, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
+        basic.showIcon(IconNames.No)
+    } else if (receivedString == "Door Closed") {
+        basic.showIcon(IconNames.Yes)
+    }
+})
 radio.setGroup(17)
 basic.forever(function () {
     if (input.magneticForce(Dimension.Strength) < 100) {
